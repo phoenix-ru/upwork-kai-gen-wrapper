b3a8d104d2984872182f54d259638b487560b251
241299 <241299@users.noreply.github.com>
2020-01-28 19:24:43 +0300

    += Changed generate handler

--- handlers/generate.handler.js
+++ handlers/generate.handler.js
@@ -19,29 +19,79 @@ async function handleGenerate(req, res) {
     return
   }
 
-  /* Fetch the necessary files */
-  const filePromises = API.fetchFiles(credentials, generatorConfiguration)
-
-  /* Supply client with the token */
+  /* The configuration was fetched successfully, so supply the client with the token */
   respond(res, { token: credentials.token })
-  // todo after that, client must establish socket connection using secret he got and we will do other things through it
-  // todo update the state for the current client
+
+  /* Socket: TODO */
+  // client must establish socket connection using secret he got and we will do other things through it
+  // update the state for the current client
+  const socket = {}
+  const stage = 0 // todo wrap into some nice interface
+  const notifyProgress = (socket, data) => {
+    /* todo notify client (or use interface) */
+    console.log(data)
+  }
+
+  /* Fetch the necessary files */
+  let filePromises = API.fetchFiles(credentials, generatorConfiguration)
+
+  /* Counter and total for watching the progress */
+  let assetsCounter = 0
+  const assetsTotal = filePromises.length
+
+  /* Watch for the progress of assets */
+  filePromises.forEach((promise) => {
+    promise.then((res) => {
+      if (res && res.error) {
+        // notify about error
+        return
+      }
+
+      notifyProgress(socket, {
+        event: 'progress',
+        stage,
+        data: {
+          count: ++assetsCounter,
+          total: assetsTotal
+        }
+      })
+    })
+  })
+
+  /* Prepare config */
+  const configBuilder = new ConfigBuilder(generatorConfiguration)
+  const assets = await Promise.all(filePromises)
+  if (assets.some(el => el.error)) {
+    // notify about error
+    // stop execution
+    return
+  }
+
+  /* Add client id */
+  configBuilder.useClientId(credentials.token)
+
+  /* Add assets */
+  configBuilder.addAssets(assets)
 
   /* Build and write config */
-  const configBuilder = new ConfigBuilder(generatorConfiguration)
-  configBuilder.addFiles(await Promise.all(filePromises))
   const builtConfig = configBuilder.build()
-  configBuilder.write()
+  await builtConfig.write()
+  notifyProgress(socket, {
+    event: 'progress',
+    stage: 1
+  })
 
   /* Setup environment variables */
-  const envConvig = {}
+  const envConvig = {
+    TOKEN: credentials.token
+  }
 
   /* Invoke generation */
   const npmInterface = new NPMInterface(builtConfig, envConvig)
   await npmInterface.runGenerate()
 
   /* Notify about success */
-  console.log('Generated from config', builtConfig)
+  // console.log('Generated from config', builtConfig)
 
   /* Clean up */
   console.log('Cleaning up')
